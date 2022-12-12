44bc53c3506ef2575911920a5d3f0ff6a71cd31d
Megan Chantosa <megan@Megans-MacBook-Air.local>
2022-12-11 23:09:10 -0800

    Completed user contact render and established query and page infrastructure for contact editting

--- lib/data/seed-data.sql
+++ lib/data/seed-data.sql
@@ -7,12 +7,12 @@ INSERT INTO addresses (street_address_1, city, state_code, zip_code, country)
   VALUES ('123 an exciting street', 'Seattle', 'WA', '98370', 'United States'),
     ('123 an exciting street', 'South Riding', 'VA', '20152', 'United States');
 
-INSERT INTO contacts (first_name, last_name, birthday, phone_number, preffered_medium, address_id, user_username)
-  VALUES ('Zoe', NULL, '2014-05-05', '360-123-7890', 'Slack', 1, 'user@domain.com'),
-    ('Riley', NULL, '2008-02-22', '360-456-7890', 'Snapchat', 1, 'user@domain.com'),
-    ('Ryan', NULL, '2003-11-29', '360-789-0123', 'phone', 2, 'user@domain.com'),
-    ('Oak', NULL, '1975-06-19', '206-123-4444', 'email', NULL, 'user@domain.com'),
-    ('Noel', NULL, '2004-12-22', NULL, NULL, NULL, 'user@domain.com');
+INSERT INTO contacts (first_name, last_name, birthday, phone_number, preferred_medium, address_id, user_id)
+  VALUES ('Zoe', NULL, '2014-05-05', '360-123-7890', 'Slack', 1, 1),
+    ('Riley', NULL, '2008-02-22', '360-456-7890', 'Snapchat', 1, 1),
+    ('Ryan', NULL, '2003-11-29', '360-789-0123', 'phone', 2, 1),
+    ('Oak', NULL, '1975-06-19', '206-123-4444', 'email', NULL, 1),
+    ('Noel', NULL, '2004-12-22', NULL, NULL, NULL, 1);
 
 INSERT INTO objectives (contact_id) SELECT id FROM contacts;
 
