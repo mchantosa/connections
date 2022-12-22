588d93b5bd0439904e991714dc22a3ceabb6daae
Megan Chantosa <megan@Megans-MacBook-Air.local>
2022-12-21 20:15:43 -0800

    I applied a linter and broke all my stuff, I am going through and debugging, this check in has recovered contacts, contact detetailed, contact create, and objective detailed

--- test/contact.test.js
+++ test/contact.test.js
@@ -1,230 +1,217 @@
-"use strict";
-
-const Contact= require("../lib/contact");
-const Objective= require("../lib/objective");
+/* eslint-disable no-undef */
+const Contact = require('../lib/contact');
+const Objective = require('../lib/objective');
 
 describe('Test: Contact', () => {
-
   describe('Test: Contact.makeContact()', () => {
-
     test('Test: Contact.makeContact() returns an empty contact with an empty objectives array', () => {
-      let contact = Contact.makeContact();
+      const contact = Contact.makeContact();
       expect(contact instanceof Contact).toBe(true);
       expect(contact.objectives).toEqual([]);
       expect(contact.getEmail()).toBe('none');
     });
 
     test('Test: Contact.makeContact(contactData, objectData) returns a contact with contact data and objectives containing objective data', () => {
-      let contactData = {notes:'Shiva bit me'};
-      let objectiveData = [
-        new Objective({ sillyData: 'I love my cat'}),
+      const contactData = { notes: 'Shiva bit me' };
+      const objectiveData = [
+        new Objective({ sillyData: 'I love my cat' }),
         new Objective({
-          id: 1, //int
-          contact_id: 1, //int
-          occasion: 'birthday', //varchar(25)
-          date_occasion: '1100-02-20', //date
-          periodicity: 'weekly', //text
-          date_next_contact: new Date(), //date
-          date_last_contact: new Date('2022/11/03'),//date
-          reminder: 'month', //text
-        })
-      ]
-      let contact = Contact.makeContact(contactData, objectiveData);
+          id: 1, // int
+          contact_id: 1, // int
+          occasion: 'birthday', // varchar(25)
+          date_occasion: '1100-02-20', // date
+          periodicity: 'weekly', // text
+          date_next_contact: new Date(), // date
+          date_last_contact: new Date('2022/11/03'), // date
+          reminder: 'month', // text
+        }),
+      ];
+      const contact = Contact.makeContact(contactData, objectiveData);
       expect(contact instanceof Contact).toBe(true);
-      expect(contact.objectives[0] instanceof Objective).toBe(true)
-      expect(contact.objectives[0].contact_id).toBe(contact.id)
-      expect(contact.objectives[1].contact_id).toBe(contact.id)
+      expect(contact.objectives[0] instanceof Objective).toBe(true);
+      expect(contact.objectives[0].contact_id).toBe(contact.id);
+      expect(contact.objectives[1].contact_id).toBe(contact.id);
       delete contact.objectives[0].contact_id;
       delete contact.objectives[1].contact_id;
       delete objectiveData[0].contact_id;
       delete objectiveData[1].contact_id;
       expect(contact.objectives).toEqual(objectiveData);
       expect(contact.getEmail()).toBe('none');
-      expect(contact.objectives[1].getOccasionDate()).toBe('02-20')
+      expect(contact.objectives[1].getOccasionDate()).toBe('02-20');
     });
-
-  })
+  });
 
   test('Test: Contact.makeContact(contactData, objectData) propagates contact.id to contact objective,contact_id', () => {
-    let contact = new Contact({id:5, notes:'Shiva bit me'});
-    let objectiveData = [
-      new Objective({ sillyData: 'I love my cat'}),
+    const contact = new Contact({ id: 5, notes: 'Shiva bit me' });
+    const objectiveData = [
+      new Objective({ sillyData: 'I love my cat' }),
       new Objective({
-        id: 1, //int
-        contact_id: 1, //int
-        occasion: 'birthday', //varchar(25)
-        date_occasion: '1100-02-20', //date
-        periodicity: 'weekly', //text
-        date_next_contact: new Date(), //date
-        date_last_contact: new Date('2022/11/03'),//date
-        reminder: 'month', //text
-      })
+        id: 1, // int
+        contact_id: 1, // int
+        occasion: 'birthday', // varchar(25)
+        date_occasion: '1100-02-20', // date
+        periodicity: 'weekly', // text
+        date_next_contact: new Date(), // date
+        date_last_contact: new Date('2022/11/03'), // date
+        reminder: 'month', // text
+      }),
     ];
     contact.objectives = objectiveData;
     contact.mountObjectives();
     expect(contact instanceof Contact).toBe(true);
-    expect(contact.objectives[0] instanceof Objective).toBe(true)
-    expect(contact.objectives[0].contact_id).toBe(contact.id)
-    expect(contact.objectives[1].contact_id).toBe(contact.id)
+    expect(contact.objectives[0] instanceof Objective).toBe(true);
+    expect(contact.objectives[0].contact_id).toBe(contact.id);
+    expect(contact.objectives[1].contact_id).toBe(contact.id);
     delete contact.objectives[0].contact_id;
     delete contact.objectives[1].contact_id;
     delete objectiveData[0].contact_id;
     delete objectiveData[1].contact_id;
     expect(contact.objectives).toEqual(objectiveData);
     expect(contact.getEmail()).toBe('none');
-    expect(contact.objectives[1].getOccasionDate()).toBe('02-20')
+    expect(contact.objectives[1].getOccasionDate()).toBe('02-20');
   });
-
-})
-
-  describe('Test Initialization', () => {
-
-    test('Test: Falsy parameter initialization', () => {
-      let contact = new Contact();
-      expect(contact.mount).toBeDefined();
-      expect(contact.mountObjectives).toBeDefined();
-      expect(contact instanceof Contact).toBe(true);
-      expect(contact.objectives).toEqual([]);
-    });
-  
-    test('Test: Random parameter initialization', () => {
-      let contact = new Contact({
-        "0" : false,
-        key1: "1", 
-        key2: 2,
-        date: new Date('2000/02/03'),
-        sum: (a, b) => { 
-          return a + b;
-        },
-      });
-      expect(contact[0]).toBe(false);
-      expect(contact.key1).toBe("1");
-      expect(contact.key2).toBe(2);
-      expect(contact.date instanceof Date).toBe(true);
-      expect(contact.sum(1, "a")).toBe("1a");
-    });
-
-    test('Test: contactData parameter initialization', () => {
-      let contact = new Contact({
-        first_name: 'Shiva',
-      });
-      expect(contact.getName()).toBe('Shiva');
-    });
-  })
-
-  describe('Test: mount(contactData) ', () => {
-
-    test('Test: contact.mount(contactData) adds contactData fields to contact', () => {
-      let contact = new Contact();
-      let contactData = {notes:'Shiva bit me'}
-      contact.mount(contactData);
-      expect(contact.notes).toBe('Shiva bit me');
-    });
-
-    test('Test: contact.mount(contactData), contactData is accessible to contact methods', () => {
-      let contact = new Contact();
-      let contactData = {notes:'Shiva bit me'}
-      contact.mount(contactData);
-      expect(contact.getInputNotes()).toBe('Shiva bit me');
-    });
-    
-  })
-
-  describe('Test: mountObjectives(objectiveData) ', () => {
-
-    test('Test: contact.mountObjectives() links a contact.id to objective.contact_id', () => {
-      let contact = new Contact({
-        id: 5,
-      });
-      contact.mountObjectives([{test : 1},{Shiva: "is naughty"}]);
-      contact.objectives.forEach(objective => {
-        expect(objective.contact_id).toBe(5);  
-      });
-    });
-   
-    test('Test: contact.mountObjectives(), if objectives are undefined, sets objectives to []', () => {
-      let contact = new Contact();
-      delete contact.objectives;
-      contact.mountObjectives();
-      expect(contact.objectives).toEqual([]);
-    });
-
-    test('Test: contact.mountObjectives() if objectives are defined, transforms objectiveData objectives into Objective objectives', () => {
-      let contact = new Contact();
-      delete contact.objectives;
-      contact.objectives = [
-        { sillyData: 'I love my cat'},
-        {
-          id: 1, //int
-          contact_id: 1, //int
-          occasion: 'birthday', //varchar(25)
-          date_occasion: '1100-02-20', //date
-          periodicity: 'weekly', //text
-          date_next_contact: new Date(), //date
-          date_last_contact: new Date('2022/11/03'),//date
-          reminder: 'month', //text
-        }
-      ]
-      contact.mountObjectives();
-      let objectivesClassMatcher = [];
-      contact.objectives.forEach(objective => {
-        objectivesClassMatcher.push(objective instanceof Objective);
-      })
-      expect(objectivesClassMatcher).toEqual([true, true])
-    });
-
-    test('Test: contact.mountObjectives(objectiveData) transforms an array of objectiveData into an array of Objectives and then sets objectives to it', () => {
-      let objectiveData = [
-        { sillyData: 'I love my cat'},
-        {
-          id: 1, //int
-          contact_id: 1, //int
-          occasion: 'birthday', //varchar(25)
-          date_occasion: '1100-02-20', //date
-          periodicity: 'weekly', //text
-          date_next_contact: new Date(), //date
-          date_last_contact: new Date('2022/11/03'),//date
-          reminder: 'month', //text
-        }
-      ]
-  
-      let contact = new Contact();
-      delete contact.objectives;
-      contact.mountObjectives(objectiveData);
-
-      let objectivesClassMatcher = [];
-      contact.objectives.forEach(objective => {
-        objectivesClassMatcher.push(objective instanceof Objective);
-      })
-      expect(objectivesClassMatcher).toEqual([true, true])
-    });
-
-    test('Test: contact.mountObjectives(objectives) results in contact.objectives having objectives derived from objectives parameter', () => {
-      let objectiveData = [
-        new Objective({ sillyData: 'I love my cat'}),
-        new Objective({
-          id: 1, //int
-          contact_id: 1, //int
-          occasion: 'birthday', //varchar(25)
-          date_occasion: '1100-02-20', //date
-          periodicity: 'weekly', //text
-          date_next_contact: new Date(), //date
-          date_last_contact: new Date('2022/11/03'),//date
-          reminder: 'month', //text
-        })
-      ]
-  
-      let contact = new Contact();
-      delete contact.objectives;
-      contact.mountObjectives(objectiveData);
-
-      let objectivesClassMatcher = [];
-      contact.objectives.forEach(objective => {
-        objectivesClassMatcher.push(objective instanceof Objective);
-      })
-      expect(contact.objectives[0].sillyData).toBe('I love my cat');
-      expect(contact.objectives[1].getOccasionDate()).toBe('02-20');
-    });
 });
 
+describe('Test Initialization', () => {
+  test('Test: Falsy parameter initialization', () => {
+    const contact = new Contact();
+    expect(contact.mount).toBeDefined();
+    expect(contact.mountObjectives).toBeDefined();
+    expect(contact instanceof Contact).toBe(true);
+    expect(contact.objectives).toEqual([]);
+  });
 
+  test('Test: Random parameter initialization', () => {
+    const contact = new Contact({
+      0: false,
+      key1: '1',
+      key2: 2,
+      date: new Date('2000/02/03'),
+      sum: (a, b) => a + b,
+    });
+    expect(contact[0]).toBe(false);
+    expect(contact.key1).toBe('1');
+    expect(contact.key2).toBe(2);
+    expect(contact.date instanceof Date).toBe(true);
+    expect(contact.sum(1, 'a')).toBe('1a');
+  });
+
+  test('Test: contactData parameter initialization', () => {
+    const contact = new Contact({
+      first_name: 'Shiva',
+    });
+    expect(contact.getName()).toBe('Shiva');
+  });
+});
+
+describe('Test: mount(contactData) ', () => {
+  test('Test: contact.mount(contactData) adds contactData fields to contact', () => {
+    const contact = new Contact();
+    const contactData = { notes: 'Shiva bit me' };
+    contact.mount(contactData);
+    expect(contact.notes).toBe('Shiva bit me');
+  });
+
+  test('Test: contact.mount(contactData), contactData is accessible to contact methods', () => {
+    const contact = new Contact();
+    const contactData = { notes: 'Shiva bit me' };
+    contact.mount(contactData);
+    expect(contact.getInputNotes()).toBe('Shiva bit me');
+  });
+});
+
+describe('Test: mountObjectives(objectiveData) ', () => {
+  test('Test: contact.mountObjectives() links a contact.id to objective.contact_id', () => {
+    const contact = new Contact({
+      id: 5,
+    });
+    contact.mountObjectives([{ test: 1 }, { Shiva: 'is naughty' }]);
+    contact.objectives.forEach((objective) => {
+      expect(objective.contact_id).toBe(5);
+    });
+  });
+
+  test('Test: contact.mountObjectives(), if objectives are undefined, sets objectives to []', () => {
+    const contact = new Contact();
+    delete contact.objectives;
+    contact.mountObjectives();
+    expect(contact.objectives).toEqual([]);
+  });
+
+  test('Test: contact.mountObjectives() if objectives are defined, transforms objectiveData objectives into Objective objectives', () => {
+    const contact = new Contact();
+    delete contact.objectives;
+    contact.objectives = [
+      { sillyData: 'I love my cat' },
+      {
+        id: 1, // int
+        contact_id: 1, // int
+        occasion: 'birthday', // varchar(25)
+        date_occasion: '1100-02-20', // date
+        periodicity: 'weekly', // text
+        date_next_contact: new Date(), // date
+        date_last_contact: new Date('2022/11/03'), // date
+        reminder: 'month', // text
+      },
+    ];
+    contact.mountObjectives();
+    const objectivesClassMatcher = [];
+    contact.objectives.forEach((objective) => {
+      objectivesClassMatcher.push(objective instanceof Objective);
+    });
+    expect(objectivesClassMatcher).toEqual([true, true]);
+  });
+
+  test('Test: contact.mountObjectives(objectiveData) transforms an array of objectiveData into an array of Objectives and then sets objectives to it', () => {
+    const objectiveData = [
+      { sillyData: 'I love my cat' },
+      {
+        id: 1, // int
+        contact_id: 1, // int
+        occasion: 'birthday', // varchar(25)
+        date_occasion: '1100-02-20', // date
+        periodicity: 'weekly', // text
+        date_next_contact: new Date(), // date
+        date_last_contact: new Date('2022/11/03'), // date
+        reminder: 'month', // text
+      },
+    ];
+
+    const contact = new Contact();
+    delete contact.objectives;
+    contact.mountObjectives(objectiveData);
+
+    const objectivesClassMatcher = [];
+    contact.objectives.forEach((objective) => {
+      objectivesClassMatcher.push(objective instanceof Objective);
+    });
+    expect(objectivesClassMatcher).toEqual([true, true]);
+  });
+
+  test('Test: contact.mountObjectives(objectives) results in contact.objectives having objectives derived from objectives parameter', () => {
+    const objectiveData = [
+      new Objective({ sillyData: 'I love my cat' }),
+      new Objective({
+        id: 1, // int
+        contact_id: 1, // int
+        occasion: 'birthday', // varchar(25)
+        date_occasion: '1100-02-20', // date
+        periodicity: 'weekly', // text
+        date_next_contact: new Date(), // date
+        date_last_contact: new Date('2022/11/03'), // date
+        reminder: 'month', // text
+      }),
+    ];
+
+    const contact = new Contact();
+    delete contact.objectives;
+    contact.mountObjectives(objectiveData);
+
+    const objectivesClassMatcher = [];
+    contact.objectives.forEach((objective) => {
+      objectivesClassMatcher.push(objective instanceof Objective);
+    });
+    expect(contact.objectives[0].sillyData).toBe('I love my cat');
+    expect(contact.objectives[1].getOccasionDate()).toBe('02-20');
+  });
+});
--- test/objective.test.js
+++ test/objective.test.js
@@ -1,160 +1,150 @@
-"use strict";
-
-const Objective= require("../lib/objective");
+/* eslint-disable no-undef */
+const Objective = require('../lib/objective');
 
 describe('Test: Objective', () => {
-
   describe('Test: Initialization', () => {
-
     test('Test: Falsy parameter initialization', () => {
-      let objective = new Objective();
+      const objective = new Objective();
       expect(objective.mount).toBeDefined();
       expect(objective.getOccasionDate).toBeDefined();
       expect(objective instanceof Objective).toBe(true);
     });
-  
+
     test('Test: Random parameter initialization', () => {
-      let objective = new Objective({
-        "0" : false,
-        key1: "1", 
+      const objective = new Objective({
+        0: false,
+        key1: '1',
         key2: 2,
         date: new Date('2000/02/03'),
-        sum: (a, b) => { 
-          return a + b;
-        },
+        sum: (a, b) => a + b,
       });
       expect(objective[0]).toBe(false);
-      expect(objective.key1).toBe("1");
+      expect(objective.key1).toBe('1');
       expect(objective.key2).toBe(2);
       expect(objective.date instanceof Date).toBe(true);
-      expect(objective.sum(1, "a")).toBe("1a");
+      expect(objective.sum(1, 'a')).toBe('1a');
     });
 
     test('Test: objectiveData parameter initialization', () => {
-      let objective = new Objective({
-        date_occasion: new Date('2000/02/03')
+      const objective = new Objective({
+        date_occasion: new Date('2000/02/03'),
       });
       expect(objective.getOccasionDate()).toBe('2000-02-03');
     });
-
-  })
+  });
 
   describe('getOccasionDate()', () => {
-
     test('Test: no this.date_occasion returns undefined', () => {
-      //Returns date_occasion of type Date as string formatted to YYYY-MM-DD
-      let objective = new Objective();
+      // Returns date_occasion of type Date as string formatted to YYYY-MM-DD
+      const objective = new Objective();
       expect(objective.getOccasionDate()).toBeUndefined();
     });
 
     test('Test: formats Date date_occasion', () => {
-      //Returns date_occasion of type Date as string formatted to YYYY-MM-DD
-      let objective = new Objective({
-        date_occasion: new Date('2000/02/03')
+      // Returns date_occasion of type Date as string formatted to YYYY-MM-DD
+      const objective = new Objective({
+        date_occasion: new Date('2000/02/03'),
       });
       expect(objective.getOccasionDate()).toBe('2000-02-03');
     });
 
     test('Test: formats string date_occasion', () => {
-      //Returns date_occasion of type string as a string formatted to YYYY-MM-DD
-      let objective = new Objective({
+      // Returns date_occasion of type string as a string formatted to YYYY-MM-DD
+      const objective = new Objective({
         date_occasion: '2000/02/03',
       });
       expect(objective.getOccasionDate()).toBe('2000-02-03');
     });
 
     test('Test: formats dates of year 1100 to MM-DD', () => {
-      //Returns date_occasion of form "1100-MM-DD" as string formatted MM-DD
-      let objective = new Objective({
+      // Returns date_occasion of form "1100-MM-DD" as string formatted MM-DD
+      const objective = new Objective({
         date_occasion: '1100-02-03',
       });
       expect(objective.getOccasionDate()).toBe('02-03');
     });
 
     test('Test: returns "Invalid date" for date_occasion of "invalid"', () => {
-      //Returns 'invalid' for bad input
-      let objective1 = new Objective({
+      // Returns 'invalid' for bad input
+      const objective1 = new Objective({
         date_occasion: '',
       });
-      let objective2 = new Objective({
+      const objective2 = new Objective({
         date_occasion: 'invalid',
       });
       expect(objective1.getOccasionDate()).toBeUndefined();
-      expect(objective2.getOccasionDate()).toBe("Invalid date");
+      expect(objective2.getOccasionDate()).toBe('Invalid date');
     });
-  })
+  });
 
   describe('Test: sanitizeDate()', () => {
-   
     test('Test: undefined', () => {
-      let objective = new Objective();
+      const objective = new Objective();
       objective.sanitizeDateOccasion();
       expect(objective.date_occasion).toBeUndefined();
     });
 
     test('Test: 3', () => {
       expect(() => {
-        let objective = new Objective({date_occasion: 3});
+        const objective = new Objective({ date_occasion: 3 });
         objective.sanitizeDateOccasion();
-      }).toThrow("No numbers!");
+      }).toThrow('No numbers!');
     });
 
     test('Test: Date object', () => {
-      let objective = new Objective({date_occasion: new Date('2022/02/20')});
+      const objective = new Objective({ date_occasion: new Date('2022/02/20') });
       objective.sanitizeDateOccasion();
       console.log(objective.date_occasion);
-      expect(objective.date_occasion).toBe("2022-02-20");
+      expect(objective.date_occasion).toBe('2022-02-20');
     });
-    
+
     test('Test: "11/29"', () => {
-      let objective = new Objective({
-        date_occasion: "11/29"
+      const objective = new Objective({
+        date_occasion: '11/29',
       });
       objective.sanitizeDateOccasion();
-      expect(objective.date_occasion).toBe("1100-11-29");
+      expect(objective.date_occasion).toBe('1100-11-29');
     });
 
     test('Test: "1-3"', () => {
-      let objective = new Objective({
-        date_occasion:"1-3"
+      const objective = new Objective({
+        date_occasion: '1-3',
       });
       objective.sanitizeDateOccasion();
-      expect(objective.date_occasion).toBe("1100-01-03");
+      expect(objective.date_occasion).toBe('1100-01-03');
     });
 
     test('Test: "2022/02/20"', () => {
-      let objective = new Objective({
-        date_occasion:"2022/02/20"
+      const objective = new Objective({
+        date_occasion: '2022/02/20',
       });
       objective.sanitizeDateOccasion();
-      expect(objective.date_occasion).toBe("2022-02-20");
+      expect(objective.date_occasion).toBe('2022-02-20');
     });
 
     test('Test: "2022-02-20"', () => {
-      let objective = new Objective({
-        date_occasion:"2022/02/20"
+      const objective = new Objective({
+        date_occasion: '2022/02/20',
       });
       objective.sanitizeDateOccasion();
-      expect(objective.date_occasion).toBe("2022-02-20");
+      expect(objective.date_occasion).toBe('2022-02-20');
     });
 
     test('Test: "2001/02/29"', () => {
-      let objective = new Objective({
-        date_occasion:"2001/02/29"
+      const objective = new Objective({
+        date_occasion: '2001/02/29',
       });
       objective.sanitizeDateOccasion();
-      expect(objective.date_occasion).toBe("invalid");
+      expect(objective.date_occasion).toBe('invalid');
     });
-
-  })
-
+  });
 });
 
-//Grab an empty test
+// Grab an empty test
 // describe('Test: something', () => {
-   
+
 //   test('Test: 1+2', () => {
 //     expect(1+2).toBe(3);
 //   });
-  
+
 // })
